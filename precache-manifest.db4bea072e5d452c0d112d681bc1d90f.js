02195ca07d134ad98c6860a1e29c57aa6d54e4a8
Nicholas Chin <nchin.nc@gmail.com>
2018-10-16 20:40:18 +0100

    Updates

---
+++ precache-manifest.db4bea072e5d452c0d112d681bc1d90f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "098837df395b1a6a7cc9",
+    "url": "/weather-app-challenge-v2/static/js/runtime~main.098837df.js"
+  },
+  {
+    "revision": "7761c526046c160eeea2",
+    "url": "/weather-app-challenge-v2/static/js/main.7761c526.chunk.js"
+  },
+  {
+    "revision": "7d7b7c4e4b577212bd9d",
+    "url": "/weather-app-challenge-v2/static/js/1.7d7b7c4e.chunk.js"
+  },
+  {
+    "revision": "7761c526046c160eeea2",
+    "url": "/weather-app-challenge-v2/static/css/main.0b1d16dd.chunk.css"
+  },
+  {
+    "revision": "76584eb8c343b2a74cc8646705a42690",
+    "url": "/weather-app-challenge-v2/index.html"
+  }
+];
